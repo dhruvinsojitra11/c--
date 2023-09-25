c7883d75dc1e23f2dcbcf3f31952e721f7a09767
unknown <dhruvinsojitra11@gmail.com>
2023-09-25 16:26:06 +0530

    second commit

---
+++ myself.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "-> my name is dhruvin" <<endl;
+    cout << "-> i have completed my bachlour " <<endl; 
+    cout << "-> now i am studing in skillqode" <<endl;
+    cout << "-> i am learning various languages of coding here" <<endl;
+    cout << "-> see my github repositories ";
+
+    return 0;
+}
