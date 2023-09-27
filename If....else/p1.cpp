74fbce069eb2c080534b1426bebe58d0da3f64d2
unknown <dhruvinsojitra11@gmail.com>
2023-09-27 17:00:32 +0530

    third commit

---
+++ If....else/p1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+int main()
+{
+    int a, b;
+    std::cout << "enter A:";
+    std::cin >> a;
+    std::cout << "enter B:";
+    std::cin >> b;
+    if(a < b)
+    {
+        std::cout << " A  Is Small Nomber  : ";
+    }
+    else
+    {
+        std::cout << " A Is Big Nomber  Is : ";
+    }
+    return 0;
+}
